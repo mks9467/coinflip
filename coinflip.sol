0321ab032960f68768d30fb8dec5d14572b8a18d
mks <66452502+mks9467@users.noreply.github.com>
2021-09-18 23:56:35 -0400

    add start() functionality

--- coinflip.sol
+++ coinflip.sol
@@ -3,21 +3,22 @@ pragma solidity ^0.8.7;
 contract coinflip {
     uint rollNumber = 1;
     uint guess = 1;
+    event Roll(address payable indexed, uint, bool);
     
-    constructor() {
+    function start() public payable {
         address payable winner = payable(msg.sender);
+        uint bet = msg.value;
+        roll(winner, bet);
     }
     
-    //function flip() public {
-        
-    //}
-    function roll(address payable _winner) private returns(bool){
+    function roll(address payable _winner, uint _bet) private {
         require(guess == 1 || guess == 2);
         if (guess == rollNumber) {
-            _winner.transfer(msg.value);
+            _winner.transfer(_bet);
+            emit Roll(_winner, _bet, true);
         }
         else {
-            return false;
+            emit Roll(_winner, _bet, false);
         }
     }
 }

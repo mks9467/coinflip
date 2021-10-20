476ab053dba970070f98f9839c669bef23a0c661
mks <66452502+mks9467@users.noreply.github.com>
2021-10-20 10:03:47 -0400

    chainlink integration part 1

--- coinflip.sol
+++ coinflip.sol
@@ -1,13 +1,65 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract RandomNumberConsumer is VRFConsumerBase {
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    uint256 public randomResult;
+    
+    /**
+     * Constructor inherits VRFConsumerBase
+     * 
+     * Network: Kovan
+     * Chainlink VRF Coordinator address: 0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9
+     * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
+     * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
+     */
+    constructor() 
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088  // LINK Token
+        )
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+    
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = (randomness % 100) + 1;
+    }
+}
 
 contract coinflip {
     uint rollNumber = 67;
     uint bet;
     address payable winner;
-    event Roll(address payable indexed, uint, bool);
+    address payable owner;
+    event Roll(address indexed, uint, bool);
+    event Deposit(address indexed, uint);
+    event Withdraw(address indexed, uint);
+    
+    constructor() {
+        owner = payable(msg.sender);
+    }
     
     function deposit() public payable {
-        
+        emit Deposit(msg.sender, msg.value);
+    }
+    
+    function withdraw(uint amount) public payable {
+        require(msg.sender == owner);
+        require(amount <= address(this).balance);
+        owner.transfer(amount);
+        emit Withdraw(msg.sender, amount);
     }
 
     function roll() public payable {
@@ -16,6 +68,7 @@ contract coinflip {
         require(rollNumber <= 100 && rollNumber >= 1);
         if (rollNumber > 50) {
             winner.transfer(bet);
+            winner.transfer(bet);
             emit Roll(winner, bet, true);
         }
         else {
@@ -23,3 +76,4 @@ contract coinflip {
         }
     }
 }
+
